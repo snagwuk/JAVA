bcaea018c2dbe5f7c359c9c08b7f5dc77998e611
snagwuk <snagwuk@gmail.com>
2019-09-23 11:32:14 +0900

    190923-1132

---
+++ src/ch03/AccuracyExample1.java
@@ -0,0 +1,19 @@
+package ch03;
+
+public class AccuracyExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int apple = 1;
+		
+		double pieceUnit = 0.1;
+		int number = 7;
+		
+		double result = apple - number * pieceUnit;
+		System.out.println("사과 한개에서");
+		System.out.println("0.7 조각을 뺴면");
+		System.out.println(result + " 조각이 남는다.");
+	}
+
+}
