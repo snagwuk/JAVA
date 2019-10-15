2e0db6b2871be22709e2783c95067f7dcad63d7d
snagwuk <snagwuk@gmail.com>
2019-10-15 11:36:01 +0900

    191015-1135

--- src/ch08/BuyerEx01.java
+++ src/ch08/BuyerEx01.java
@@ -1,11 +1,21 @@
 package ch08;
 
+import java.util.Vector;
+
 class Buyer
 {
     public int money = 1000;
     
     public int bonusPoint = 0;
     
+    @SuppressWarnings("rawtypes")
+    //Product[] item = new Product[10];
+    //int i = 0 ;
+    Vector vec = new Vector();
+    
+    
+    
+    @SuppressWarnings("unchecked")
     void buy(Product p)
     {
         if (money < p.price)
@@ -17,8 +27,46 @@ class Buyer
         System.out.println(p + "를 구매하였습니다.");
         money -= p.price;
         bonusPoint += p.bonusPoint;
+        //item[i++] = p;
+        vec.add(p);
+    }
+
+    public void summary()
+    {
+        int sum =0;
+        /*for(int j = 0; j < i; j++)
+        {   
+            if(item[j].price != 0)
+                System.out.println(item[j] + " / " + item[j].price + " / " +  item[j].bonusPoint);
+            sum +=item[j].price;
+        }*/
+        for(int i = 0; i < vec.size(); i++)
+        {
+            Product pp = (Product) vec.get(i);
+            System.out.println(pp + " / " + pp.price + " / " +  pp.bonusPoint);
+           sum+=pp.price;
+        }
+
+        System.out.println("총구매금액 : " + sum);
+        
         
     }
+    
+    void refund(Product p)
+    {
+
+            if(vec.remove(p))
+            {
+                money += p.price;
+                bonusPoint -= p.bonusPoint;
+/*                x.price = 0;
+                x.bonusPoint = 0;*/
+                System.out.println(p + "를 반품하였습니다.");
+            }
+        
+    }
+        
+    
 }
 
 class Product
@@ -70,9 +118,15 @@ public class BuyerEx01
     {
         // TODO Auto-generated method stub
         Buyer b = new Buyer();
-        b.buy(new Tv());
-        b.buy(new Computer());
+        Tv tv = new Tv();
+        Computer com = new Computer();
         
+        b.buy(tv);
+        b.buy(com);
+        
+        b.summary();
+        b.refund(com);
+        b.summary();
         System.out.println("현재남은돈은 : " + b.money + "만원입니다.");
         System.out.println("현재보너스점수는 : " + b.bonusPoint + "점입니다.");
     }
---
+++ src/ch08/StaticImportExam.java
@@ -0,0 +1,18 @@
+package ch08;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+public class StaticImportExam
+{
+    
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+        out.println(random());
+        
+        out.println("Math.PI : " + PI);
+        
+    }
+    
+}
